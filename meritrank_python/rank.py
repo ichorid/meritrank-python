a20f4564a0014b80db01b6ee63eb5e7aca060e3c
V.G. Bulavintsev <golem.md@gmail.com>
2022-11-27 20:12:08 +0100

    Incremental adding edges works

--- meritrank_python/rank.py
+++ meritrank_python/rank.py
@@ -14,6 +14,10 @@ class NodeId(int):
     pass
 
 
+class NodeDoesNotExist(Exception):
+    pass
+
+
 class RandomWalk(List[NodeId]):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -32,8 +36,10 @@ class WalksStorage:
     def __init__(self):
         self._walks: Dict[NodeId, Dict[uuid.UUID, PosWalk]] = {}
 
-    def add_walk(self, walk: RandomWalk):
+    def add_walk(self, walk: RandomWalk, start_pos: int = 0):
         for pos, node in enumerate(walk):
+            if pos < start_pos:
+                continue
             # Get existing walks for the node. If there is none,
             # create an empty dictionary for those
             walks_with_node = self._walks[node] = self._walks.get(node, {})
@@ -48,27 +54,26 @@ class WalksStorage:
             # To prevent linear scanning, we store the node's position in the walk
             walks_with_node[walk.uuid] = PosWalk(pos, walk)
 
-    def invalidate_walks_through_node(self, node: NodeId) -> List[RandomWalk]:
+    def invalidate_walks_through_node(self, invalidated_node: NodeId) -> \
+            List[Tuple[RandomWalk, RandomWalk]]:
         invalidated_walks = []
-        for uid, pos_walk in self._walks[node].items():
+        for uid, pos_walk in self._walks.get(invalidated_node, {}).items():
             pos, walk = pos_walk.pos, pos_walk.walk
             # For every node mentioned in the invalidated subsequence,
             # remove the corresponding entries in the bookkeeping dict
-            invalidated_subsequence = walk[pos:]
-            # FIXME: MOVE COUNTER?
-            for affected_node in invalidated_subsequence:
-                # if there is a self-reference (a loop) in the walk, don't
-                # remove it here, for not to shoot ourselves in the foot.
+            invalidated_fragment = walk[pos + 1:]
+            for affected_node in invalidated_fragment:
+                # if a node is encountered in the walk more than once, don't
+                # remove it, for not to shoot ourselves in the foot.
                 # Also, don't try to remove the walk twice from the same node
-                if (
-                        affected_node != node and
+                if (affected_node != invalidated_node and
                         affected_node in self._walks and
                         uid in self._walks[affected_node]):
                     self._walks[affected_node].pop(uid)
 
             # Remove the invalidated subsequence from the walk
-            del walk[pos:]
-            invalidated_walks.append(walk)
+            del walk[pos + 1:]
+            invalidated_walks.append((walk, invalidated_fragment))
         return invalidated_walks
 
     def get_walks(self, node):
@@ -84,6 +89,9 @@ class IncrementalPageRank:
         self.max_iter = max_iter
 
     def calculate(self, src: NodeId):
+        if not self._graph.has_node(src):
+            raise NodeDoesNotExist
+
         counter = self._personal_hits[src] = Counter()
         for _ in range(0, self.max_iter):
             walk = self.perform_walk(src)
@@ -101,22 +109,33 @@ class IncrementalPageRank:
         return {node: count / total for node, count in counter.items()}
 
     def perform_walk(self, start_node: NodeId) -> RandomWalk:
-        return self.continue_walk(RandomWalk([start_node]))
+        walk = RandomWalk([start_node])
+        self.continue_walk(walk)
+        return walk
 
-    def continue_walk(self, walk: RandomWalk) -> RandomWalk:
+    def continue_walk(self, walk: RandomWalk):
         while ((neighbours := list(self._graph.neighbors(node := walk[-1])))
                and random.random() <= self.alpha):
             weights = [self._graph[node][nbr]['weight'] for nbr in neighbours]
             walk.append(random.choices(neighbours, weights=weights, k=1)[0])
-        return walk
 
     def add_edge(self, src: NodeId, dst: NodeId, weight: float = 1.0):
         self._graph.add_edge(src, dst, weight=weight)
         invalidated_walks = self._walks.invalidate_walks_through_node(src)
-        for walk in invalidated_walks:
+        for (walk, invalidated_fragment) in invalidated_walks:
+            # Subtract the nodes in the invalidated sequence from the hit counter
+            # for the starting node of the invalidated walk.
+            starting_node = walk[0]
+            counter = self._personal_hits[starting_node]
+            counter.subtract(invalidated_fragment)
+
+            # Finish the invalidated walk. The stochastic nature of the random walk
+            # allows us to complete the walk by just continuing it until it stops naturally.
+            new_fragment_start = len(walk)
             self.continue_walk(walk)
-            # FIXME !!!!!!!!!!!!!! continuation
-            self._walks.add_walk(walk)
+            counter.update(walk[new_fragment_start:])
+
+            self._walks.add_walk(walk, start_pos=new_fragment_start)
 
     def calc_rank(self, node):
         pass
--- tests/test_rank.py
+++ tests/test_rank.py
@@ -1,10 +1,33 @@
 import random
+from operator import itemgetter
 
 import networkx as nx
 import pytest
 from _pytest.python_api import approx
+from networkx import scale_free_graph
 
-from meritrank_python.rank import RandomWalk, PosWalk, WalksStorage, IncrementalPageRank
+from meritrank_python.rank import RandomWalk, PosWalk, WalksStorage, IncrementalPageRank, NodeDoesNotExist
+
+
+def top_items(d, num_items=3):
+    return dict(sorted(d.items(), key=itemgetter(1), reverse=True)[:num_items])
+
+
+def assert_ranking_approx(d1, d2, num_top_items=5, precision=0.1):
+    """
+    Compare the first num_items of two ranking dicts with given precision
+    """
+
+    assert top_items(d1, num_top_items) == approx(top_items(d2, num_top_items), rel=precision)
+
+
+def test_assert_ranking_approx():
+    assert_ranking_approx({1: 0.3}, {1: 0.31})
+
+
+@pytest.fixture
+def rng():
+    random.seed(1)
 
 
 @pytest.fixture
@@ -15,8 +38,15 @@ def simple_graph():
     )
 
 
+def get_scale_free_graph(count):
+    graph = scale_free_graph(count)
+    weighted_graph = nx.DiGraph(seed=123)
+    for edge in graph.edges():
+        weighted_graph.add_edge(edge[0], edge[1], weight=1.0)
+    return weighted_graph
+
+
 def test_pagerank(simple_graph):
-    random.seed(1)
     # rank = nx.pagerank(simple_graph, personalization={0: 1})
     # print(rank)
     ipr = IncrementalPageRank(simple_graph)
@@ -28,12 +58,65 @@ def test_pagerank(simple_graph):
 
 
 def test_pagerank_incremental(simple_graph):
-    ipr = IncrementalPageRank(simple_graph)
-    ipr.calculate(0)
-    ranks1 = ipr.get_ranks(0)
+    ipr1 = IncrementalPageRank(simple_graph)
+    ipr1._graph.add_edge(2, 3, weight=1.0)
+    ipr1.calculate(0)
+    ranks_simple = ipr1.get_ranks(0)
 
-    ipr.add_edge(2, 3)
-    ranks2 = ipr.get_ranks(0)
+    ipr2 = IncrementalPageRank(simple_graph)
+    ipr2.calculate(0)
+    ipr2.add_edge(2, 3)
+    ranks_incremental = ipr2.get_ranks(0)
+
+    assert ranks_incremental == ranks_simple
+
+
+def test_pagerank_incremental_basic():
+    graph = nx.DiGraph(
+        {0: {1: {'weight': 1}}}
+    )
+    ipr1 = IncrementalPageRank(graph)
+    ipr1._graph.add_edge(0, 2, weight=1.0)
+    ipr1.calculate(0)
+    ranks_simple = ipr1.get_ranks(0)
+
+    ipr2 = IncrementalPageRank(graph)
+    ipr2.calculate(0)
+    ipr2.add_edge(0, 2)
+    ranks_incremental = ipr2.get_ranks(0)
+
+    assert ranks_incremental == ranks_simple
+
+
+def test_calculate_nonexistent_node(simple_graph):
+    ipr1 = IncrementalPageRank()
+    with pytest.raises(NodeDoesNotExist):
+        ipr1.calculate(0)
+
+
+def test_pagerank_incremental_from_empty_graph():
+    ipr1 = IncrementalPageRank()
+    ipr1._graph.add_node(0)
+    ipr1.calculate(0)
+    ipr1.add_edge(0, 1, weight=1.0)
+    ipr1.add_edge(0, 2, weight=1.0)
+    print(ipr1.get_ranks(0))
+
+
+def test_pagerank_incremental_big(rng):
+    graph = get_scale_free_graph(1000)
+    ipr1 = IncrementalPageRank(graph, max_iter=1000)
+    ipr1.calculate(0)
+    ranks_simple = ipr1.get_ranks(0)
+
+    ipr2 = IncrementalPageRank(max_iter=1000)
+    ipr2._graph.add_node(0)
+    ipr2.calculate(0)
+    for edge in graph.edges():
+        ipr2.add_edge(edge[0], edge[1], weight=1.0)
+    ranks_incremental = ipr2.get_ranks(0)
+
+    assert_ranking_approx(ranks_simple, ranks_incremental)
 
 
 def test_random_walk_uuid():
